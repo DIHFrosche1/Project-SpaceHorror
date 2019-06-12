2262ece868acdce98207f196e94a6162c57bf903
Churu <git@bitbucket.org:Churuu/unity.git>
2019-06-12 14:06:39 +0200

    HDRI Sky

--- Project Phoenix/Assets/Scripts/AI/HuntState.cs
+++ Project Phoenix/Assets/Scripts/AI/HuntState.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 
 /// <summary>
 /// Hunt state is used to scare the player by getting the AI as close to the player as possible, forcing the player to hide in fear of being found
-/// After hunt state is over the AI will go into patrol state
+/// After hunt state is over the AI will go into patrol state 
 /// </summary>
 
 public class HuntState : IEnemyMicroState
