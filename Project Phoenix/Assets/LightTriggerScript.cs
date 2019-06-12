3e0e0ad415317b1f754629c9b4e77f4a6e6636f6
LAPTOP-KUMTRKSA\willi <willoswe99@gmail.com>
2019-06-12 14:52:55 +1000

    Added in monster model, build map lights

---
+++ Project Phoenix/Assets/LightTriggerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTriggerScript : MonoBehaviour
+{
+    public Light corridorLight;
+
+
+    private void Start()
+    {
+        corridorLight.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!Input.GetKey(KeyCode.W))
+        {
+
+           
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+
+            corridorLight.gameObject.SetActive(true);
+            Debug.Log("Player Touched Trigger");
+
+        }
+        if (other.name == "Monster")
+        {
+            corridorLight.gameObject.SetActive(true);
+            Debug.Log("Monster TouchedTrigger");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+
+            corridorLight.gameObject.SetActive(false);
+
+        }
+        if (other.name == "Monster")
+        {
+            corridorLight.gameObject.SetActive(false);
+            Debug.Log("Monster TouchedTrigger");
+        }
+    }
+}
---
+++ Project Phoenix/Assets/playerposistionCopyer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerposistionCopyer : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform monster;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        monster.transform.position = player.transform.position + offset;
+
+        if (!Input.GetKey(KeyCode.W))
+        {
+            offset.z -= 0.01f;
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            offset.z = 20;
+        }
+
+
+    }
+}
