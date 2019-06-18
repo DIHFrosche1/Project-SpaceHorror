6b5041942afc2a9db885673a36728f94093892d4
Churu <git@bitbucket.org:Churuu/unity.git>
2019-06-18 14:23:28 +0200

    Added patrolling and chasing player

--- Project Phoenix/Assets/Scripts/AI/ChaseState.cs
+++ Project Phoenix/Assets/Scripts/AI/ChaseState.cs
@@ -6,6 +6,11 @@ using UnityEngine.AI;
 public class ChaseState : IEnemyMicroState
 {
 
+    const float m_GiveUpTimerDelta = 5f;
+    float m_GiveUpTimer;
+
+    public Transform m_LastKnowPosition;
+
     private readonly MicroAI m_MicroAI;
 
     public ChaseState(MicroAI microAI)
@@ -13,8 +18,14 @@ public class ChaseState : IEnemyMicroState
         m_MicroAI = microAI;
     }
 
+    public void Enter()
+    {
+        m_MicroAI.m_agent.speed = m_MicroAI.m_ChaseSpeed;
+    }
+
     public void UpdateState()
     {
+        ChasePlayer();
     }
 
     public void OnTriggernEnter(Collider other)
@@ -25,5 +36,26 @@ public class ChaseState : IEnemyMicroState
     public void MoveToState(IEnemyMicroState state)
     {
         m_MicroAI.m_currentAIState = state;
+        m_MicroAI.m_currentAIState.Enter();
+    }
+
+    void ChasePlayer()
+    {
+        if (m_MicroAI.CanSeePlayer())
+        {
+            m_LastKnowPosition = m_MicroAI.m_PlayerToHunt.transform;
+            m_GiveUpTimer = 5;
+        }
+        else
+        {
+            m_GiveUpTimer -= Time.deltaTime;
+            if(m_GiveUpTimer < 0)
+            {
+                MoveToState(m_MicroAI.m_HuntState);
+            }
+        }
+
+        m_MicroAI.m_agent.SetDestination(m_LastKnowPosition.position);
+        m_MicroAI.m_agent.isStopped = false;
     }
 }
--- Project Phoenix/Assets/Scripts/AI/MacroAI.cs
+++ Project Phoenix/Assets/Scripts/AI/MacroAI.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class MacroAI : MonoBehaviour
 {
+
+    public GameObject m_MicroAIObj;
+
+    private float m_WaitTimer = 5;
+    private MicroAI m_MicroAI;
+
     void Start()
     {
-        
+        m_MicroAI = m_MicroAIObj.GetComponent<MicroAI>();
     }
 
     void Update()
     {
-        
+        if (m_MicroAI.m_currentAIState == m_MicroAI.m_PatrolState)
+            GiveMicroPatrolPoints();
+    }
+
+    void GiveMicroPatrolPoints()
+    {
+        var microAgent = m_MicroAI.m_agent;
+
+        if (microAgent.remainingDistance < 1 && microAgent.pathPending == false)
+        {
+            m_WaitTimer -= Time.deltaTime;
+            microAgent.isStopped = true;
+            if (m_WaitTimer < 0)
+            {
+                int randomPatrolPoint = Random.Range(0, m_MicroAI.m_PatrolPoints.Length);
+                microAgent.SetDestination(m_MicroAI.m_PatrolPoints[randomPatrolPoint].position);
+                m_WaitTimer = 5;
+                microAgent.isStopped = false;
+            }
+        }
+    }
+
+    void GiveMicroPosition(Vector3 position)
+    {
+        m_MicroAI.m_PatrolState.PatrolOnPoint(position);
     }
 }
